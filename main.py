68247702d6a172fc69cd6ab8a43ed23e6a7f0960
kk2ktr2011-cloud <kk2ktr2011@gmail.com>
2026-07-24 16:11:34 -0500

    Added Ability for Multiple Functions

--- main.py
+++ main.py
@@ -1,33 +1,49 @@
+import json
+
 print("This application will allow you to save tasks and put their priorty and completion.")
 
 tasks = []
 
 while True:
-    taskname = ""
-    datedue = ""
-    priority = ""
-    completion = ""
+    
+    action = input("Type in whether you want to add or edit tasks (use add or edit): ")
+    
+    if action.lower() == "add":
+        taskname = ""
+        datedue = ""
+        priority = ""
+        completion = ""
 
-    while taskname == "": 
-        taskname = input("Type in a task: ")
+        while taskname == "": 
+            taskname = input("Type in a task: ")
 
-    while datedue == "":
-        datedue = input("What is the date it is due (Format: MM/DD/YY): ")
+        while datedue == "":
+            datedue = input("What is the date it is due (Format: MM/DD/YY): ")
 
-    while priority not in ["Low", "Medium", "High"]:
-        priority = input("What is the priority of the task (Low, Medium, High): ")
+        while priority not in ["Low", "Medium", "High"]:
+            priority = input("What is the priority of the task (Low, Medium, High): ")
 
-    while completion not in ["True", "False"]:
-        completion = input("Has it been completed or not (True or False): ")
+        while completion not in ["True", "False"]:
+            completion = input("Has it been completed or not (True or False): ")
 
-    task = {
-        "name": taskname,
-        "datedue": datedue,
-        "priority": priority,
-        "completion": completion
-    }
+        task = {
+            "name": taskname,
+            "datedue": datedue,
+            "priority": priority,
+            "completion": completion
+        }
 
-    tasks.append(task)
+        tasks.append(task)
+    
+    elif action.lower() == "edit":
+        if not tasks:
+            print("There are no tasks. ")
+            continue
+        print(tasks)
+
+    else:
+        action = input("Invalid Input. Try Again. ")
+        continue
 
     continue_or_not = input("Continue? Y/N: ")
     if continue_or_not.lower() == "y":
