20180a0c01c050407f0f7a7e865765a221f6c16b
kk2ktr2011-cloud <kk2ktr2011@gmail.com>
2026-07-24 11:37:29 -0500

    Added Task Adder

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 print("This application will allow you to save tasks and put their priorty and completion.")
 
+tasks = []
+
 while True:
+    taskname = ""
+    datedue = ""
+    priority = ""
+    completion = ""
+
+    while taskname == "": 
+        taskname = input("Type in a task: ")
+
+    while datedue == "":
+        datedue = input("What is the date it is due (Format: MM/DD/YY): ")
+
+    while priority not in ["Low", "Medium", "High"]:
+        priority = input("What is the priority of the task (Low, Medium, High): ")
+
+    while completion not in ["True", "False"]:
+        completion = input("Has it been completed or not (True or False): ")
+
+    task = {
+        "name": taskname,
+        "datedue": datedue,
+        "priority": priority,
+        "completion": completion
+    }
+
+    tasks.append(task)
+
     continue_or_not = input("Continue? Y/N: ")
     if continue_or_not.lower() == "y":
         continue
@@ -9,8 +37,6 @@ while True:
     else:
         continue_or_not = input("Invalid Input. Try again. Y/N: ")
 
+print(tasks)
+
 print("This is the end of this instance of the TO-DO List. ")
-
-tasks = {
-
-}
